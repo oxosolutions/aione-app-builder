5cc7dffb4493790c38a68e6fcba481418a300f31
unknown <sgs.sandhu@gmail.com>
2017-04-12 10:05:06 +0530

    custom search

--- library/aione-templates/templates/archive-template.php
+++ library/aione-templates/templates/archive-template.php
@@ -28,59 +28,143 @@
 						}
 					}
 				}
-				
-				 $args = array(
-					'posts_per_page'   => 10,
-					'post_type'        => $post_type,
-					'post_status'      => 'publish',
-					//'category_name' => 'uncategorized',
-					'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
-				);
-				/*$posts_array = get_posts( $args );
-				if($posts_array){
-					foreach($posts_array as $post_object){
-						$data = do_shortcode( $template_post->post_content );
-						$css = get_post_meta($template_post->ID, 'pyre_custom_css',true);
-						$data .= "<style>";
-						$data .= $css;
-						$data .= "</style>";
-						//echo $data;
-						//echo "<hr>";
+				if(isset($_GET['aione_search_filter']) && $_GET['aione_search_filter'] == "aione_search_filter"){
+					//$meta_array = array('relation' => 'OR',);
+					$meta_array = array();
+					
+					$raw_array = $_GET;
+					$args = array(
+						'posts_per_page'   => 10,
+						'post_type'        => $post_type,
+						'post_status'      => 'publish',
+						'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
+						
+					);
+					if($_GET['by_cat']){
+						$args['tax_query'] = array(
+												array(
+													'taxonomy' => 'category',
+													'field'    => 'slug',
+													'terms'    => $_GET['by_cat'],
+													
+												),
+											);
 					}
-				} */
-				$loop = new WP_Query( $args );
-				if( $loop->have_posts() ):
-							
-					while( $loop->have_posts() ): $loop->the_post(); global $post;
-					$data = do_shortcode( $template_post->post_content );
-						$css = get_post_meta($template_post->ID, 'pyre_custom_css',true);
-						$data .= "<style>";
-						$data .= $css;
-						$data .= "</style>";
-						echo $data;
-						echo "<hr>";
-					endwhile;
+					if($_GET['keyword']){
+						$args['s'] = $_GET['keyword'];
+					}
+					unset($raw_array['aione_search_filter']);
+					unset($raw_array['aione-search-filter-submit']);
+					unset($raw_array['by_cat']);
+					unset($raw_array['keyword']);
+					foreach($raw_array as $raw_array_key => $raw_array_val){
+						if(!empty($raw_array_val)){
+							$dump_array = array(
+								'key'     => $raw_array_key,
+								'value' => $raw_array_val,
+								'compare' => '=',
+							);
+							array_push($meta_array,$dump_array);
+						}
+						
+					}
+					//echo "<pre>";print_r($meta_array);
+					$args['meta_query'] = $meta_array;
+					/*$args = array(
+						'posts_per_page'   => 10,
+						'post_type'        => $post_type,
+						'post_status'      => 'publish',
+						'meta_query'   =>$meta_array,
+						'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
+					);*/
 					
-					global $wp_query;
-
-					$big = 999999999; // need an unlikely integer
-					$translated = __( 'Page', 'mytextdomain' ); // Supply translatable string
-					echo '<div class="pagenav">';
-					echo paginate_links( array(
-						'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						'format' => '?paged=%#%',
-						'current' => max( 1, get_query_var('paged') ),
-						'total' => $wp_query->max_num_pages,
-							'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
-					) );
-					echo '</div>';
-
-					?>
-					
-					
-					<?php
-				endif;
+				} else {
+					$args = array(
+						'posts_per_page'   => 10,
+						'post_type'        => $post_type,
+						'post_status'      => 'publish',
+						//'category_name' => 'uncategorized',
+						'paged' => ( get_query_var('paged') ? get_query_var('paged') : 1),
+					);
+				}
 				
+				
+				$loop = new WP_Query( $args );
+				//echo "<pre>";print_r($args);
+				$css = get_post_meta($template_post->ID, 'pyre_custom_css',true);
+				$sidebar = get_post_meta($template_post->ID, 'sbg_selected_sidebar_replacement',true);
+				if(empty($sidebar)){
+					if( $loop->have_posts() ):
+							
+						while( $loop->have_posts() ): $loop->the_post(); global $post;
+						$data = do_shortcode( $template_post->post_content );
+							//$css = get_post_meta($template_post->ID, 'pyre_custom_css',true);
+							//$sidebar = get_post_meta($template_post->ID, 'sbg_selected_sidebar_replacement',true);
+							$data .= "<style>";
+							$data .= $css;
+							$data .= "</style>";
+							echo $data;
+							echo "<hr>";
+						endwhile;
+						
+						global $wp_query;
+
+						$big = 999999999; // need an unlikely integer
+						$translated = __( 'Page', 'mytextdomain' ); // Supply translatable string
+						echo '<div class="pagenav">';
+						echo paginate_links( array(
+							'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+							'format' => '?paged=%#%',
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $wp_query->max_num_pages,
+								'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
+						) );
+						echo '</div>';
+
+						?>
+						<?php
+					endif;
+				} else {
+					echo "<div style='float:left;width:60%;'>";
+					if( $loop->have_posts() ):
+								
+						while( $loop->have_posts() ): $loop->the_post(); global $post;
+						$data = do_shortcode( $template_post->post_content );
+							//$css = get_post_meta($template_post->ID, 'pyre_custom_css',true);
+							//$sidebar = get_post_meta($template_post->ID, 'sbg_selected_sidebar_replacement',true);
+							$data .= "<style>";
+							$data .= $css;
+							$data .= "</style>";
+							echo $data;
+							
+							echo "<hr>";
+						endwhile;
+						
+						global $wp_query;
+
+						$big = 999999999; // need an unlikely integer
+						$translated = __( 'Page', 'mytextdomain' ); // Supply translatable string
+						echo '<div class="pagenav">';
+						echo paginate_links( array(
+							'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+							'format' => '?paged=%#%',
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $wp_query->max_num_pages,
+								'before_page_number' => '<span class="screen-reader-text">'.$translated.' </span>'
+						) );
+						echo '</div>';
+
+						?>
+						<?php
+					endif;
+					echo "</div>";
+					?>
+					<div id="sidebar" <?php Aione()->layout->add_class( 'sidebar_1_class' ); ?> >
+					<?php
+					generated_dynamic_sidebar();
+					//echo "<div style='float:right;width:40%;'>".generated_dynamic_sidebar()."</div>";
+					echo "<div style='clear:both;'></div>";
+				}
 			?>
 
 		</div><!-- #content -->
