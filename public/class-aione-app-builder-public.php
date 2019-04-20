0e693999be31f9fdc3b78f0330956325be30d37f
SGS Sandhu <sgs.sandhu@gmail.com>
2019-04-20 15:44:59 +0530

    Update

--- public/class-aione-app-builder-public.php
+++ public/class-aione-app-builder-public.php
@@ -2153,10 +2153,8 @@ class Aione_App_Builder_Public {
 		return $output;
 	} // END aione_app_builder_posts_shortcode()
 
-	public function aione_app_builder_post_shortcode( $atts, $content = null ) {
+		public function aione_app_builder_post_shortcode( $atts, $content = null ) {
 		
-		global $post, $theme_options;
-
 		// Attributes
 		$atts = shortcode_atts( array(
 			'post_id'	=> $post->ID,
@@ -2165,11 +2163,14 @@ class Aione_App_Builder_Public {
 			'class'		=> ''	
 		), $atts, 'post');
 
+		global $post, $theme_options;
+
 		$atts = $this->clean_shortcode_parameters( $atts );
 
 		$output = "";
 
 		$post =  get_post( $atts['post_id'] );
+		setup_postdata( $post ); 
 
 		$is_template = false;
 
@@ -2200,6 +2201,8 @@ class Aione_App_Builder_Public {
 		} else{
 			$output .= '<h5 class="font-size-16 align-center">Post does not exist</h5>';
 		}
+
+		wp_reset_postdata();
 		
 		return $output;
 	} // END aione_app_builder_post_shortcode()
@@ -3738,92 +3741,101 @@ class Aione_App_Builder_Public {
 
 				case "post_object":
 
-					
-
 					$post_template = $atts['template'];
 
 					$aione_templates 		= get_option( 'aione-templates' );
-					$post_template_array 		= $aione_templates[$post_template];
+					$post_template_array 	= $aione_templates[$post_template];
 
-					if($field['multiple'] == '1'){				
+					if( $field['multiple'] == '1' ) {				
 						
-						if($field['return_format'] == "object"){
-							foreach ($data as $key => $value) {
+						if( $field['return_format'] == "object" ) {
 
-								if( !empty( $post_template ) && $post_template_array ){
+							foreach ( $data as $key => $value ) {
+
+								if( !empty( $post_template ) && $post_template_array ) {
+
 									global $post;
-									/*$post_type = get_post_type( $value->ID );
-
-									$posts = get_posts( 
-										array( 
-											'include' => array( $value->ID ), 
-											'post_type' => $post_type
-										) 
-									);
-
-									if ( $posts ) {
-										foreach ( $posts as $post ) : 
-											setup_postdata( $post ); 
-											$output .= do_shortcode( $post_template_array['content'] );
-										endforeach;
-										wp_reset_postdata();
-									}*/
-
-									$post = get_post($value->ID);
+									$post = get_post( $value->ID );
 									setup_postdata( $post ); 
 									$output .= do_shortcode( $post_template_array['content'] );
 									wp_reset_postdata();
-
-								} elseif( $field['return_format'] == "object" ){
 
 								} else {
 									
 									$output .= '<div class="post-title">'.$value->post_title.'</div>';
 									$output .= '<div class="post-content">'.do_shortcode( $value->post_content ).'</div>';
-								}							
-							}				    		
+								}	
+
+							}		
+
 						} else {
-							foreach ($data as $key => $value) {
+
+							$post_ids = array();
+
+							foreach ( $data as $key => $value ) {
+
 								if( !empty( $post_template ) && $post_template_array ){
+
 									global $post;
 									$post = get_post($value);
 									setup_postdata( $post ); 
 									$output .= do_shortcode( $post_template_array['content'] );
 									wp_reset_postdata();
+
 								} else {
-									$value = get_post($value);
-									$output .= '<div class="post-title">'.$value->post_title.'</div>';
-									$output .= '<div class="post-content">'.do_shortcode( $value->post_content ).'</div>';
+
+									$post_ids[] = $value;
+
 								}
 								
 							}
+
+							$output .= implode( ',', $post_ids );
+
 						}
-					} else {			    	
-						if($field['return_format'] == "object"){
-							if( !empty( $post_template ) && $post_template_array ){
+
+					} else {
+
+						if( $field['return_format'] == "object" ) {
+
+							if( !empty( $post_template ) && $post_template_array ) {
+
 								global $post;
 								$post = get_post($data);
 								setup_postdata( $post ); 
 								$output .= do_shortcode( $post_template_array['content'] );
 								wp_reset_postdata();
+
 							} else {
+
 								$output .= '<div class="post-title">'.$data->post_title.'</div>';
 								$output .= '<div class="post-content">'.do_shortcode( $data->post_content ).'</div>';
-							}						
+
+							}	// end  else part if( !empty( $post_template ) && $post_template_array )
+
 						} else {
-							if( !empty( $post_template ) && $post_template_array ){
+
+							// $field['return_format'] is post ID
+
+							if( !empty( $post_template ) && $post_template_array ) {
+
+								// $field['return_format'] is post ID
+
 								global $post;
-								$post = get_post($data);
+								$post = get_post( $data );
 								setup_postdata( $post ); 
 								$output .= do_shortcode( $post_template_array['content'] );
 								wp_reset_postdata();
+
 							} else {
-								$data = get_post($data);
-								$output .= '<div class="post-title">'.$data->post_title.'</div>';
-								$output .= '<div class="post-content">'.do_shortcode( $data->post_content ).'</div>';
+
+								$output .= $data;
+
 							}
-						}	
-					}
+
+						} // end  else part if( $field['return_format'] == "object" ) 
+
+					} // end  else part if( $field['multiple'] == '1' ) 
 					break;
 
 
